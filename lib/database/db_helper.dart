f7dee001d81aeeebe466db0ad2040f356718c3f6
aladadev <aladadev29@gmail.com>
2026-01-11 17:56:21 +0600

    location module done

--- lib/database/db_helper.dart
+++ lib/database/db_helper.dart
@@ -47,4 +47,64 @@ class DBHelper {
     final id = await db.insert(sqlTableLocations, location.toJson());
     return location.copy(id: id);
   }
+
+  Future<LocationModel?> readLocation(int id) async {
+    final db = await instance.database;
+
+    final maps = await db.query(
+      sqlTableLocations,
+      columns: LocationFields.values,
+      whereArgs: [id],
+    );
+    if (maps.isNotEmpty) {
+      return LocationModel.fromJson(maps.first);
+    } else {
+      return null;
+    }
+  }
+
+  Future<List<LocationModel>> readAllLocations() async {
+    final db = await instance.database;
+    const orderBy = '${LocationFields.timestamp} DESC';
+
+    final result = await db.query(sqlTableLocations, orderBy: orderBy);
+    return result.map((json) => LocationModel.fromJson(json)).toList();
+  }
+
+  Future<int> getLocationCount() async {
+    final db = await instance.database;
+    final result = await db.rawQuery('SELECT COUNT(*) FROM $sqlTableLocations');
+    int? count = Sqflite.firstIntValue(result);
+    return count ?? 0;
+  }
+
+  Future<int> update(LocationModel location) async {
+    final db = await instance.database;
+    return db.update(
+      sqlTableLocations,
+      location.toJson(),
+      where: '${LocationFields.id} =?',
+      whereArgs: [location.id],
+    );
+  }
+
+  Future<int> delete(int id) async {
+    final db = await instance.database;
+
+    return await db.delete(
+      sqlTableLocations,
+      where: '${LocationFields.id} = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future<int> deleteAll() async {
+    final db = await instance.database;
+    return await db.delete(sqlTableLocations);
+  }
+
+  Future close() async {
+    final db = await instance.database;
+    db.close();
+  }
 }
